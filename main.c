2594e9c23f544c1fa0160ce656b36922b222d937
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2025-09-26 23:20:49 +0200

    Feat: Following, the diagram
    Main is giving the timer module, the hand for initialise the logic.

--- main.c
+++ main.c
@@ -1,26 +1,9 @@
 #include "my_timer.h"
-#include "my_thread_monitor.h"
 #include <stdio.h>
-typedef struct callback_args callback_args;
 int main()
 {
 	my_timer *my_timer = new_my_timer();
-	my_thread_monitor *my_thread_monitor = new_my_thread_monitor();
-
-	my_thread_monitor->init_thread_sync(my_thread_monitor);
-	my_thread_monitor->init_thread(my_thread_monitor, 0,
-				       my_timer->update_ss, "update_ss",
-				       my_timer);
-	my_thread_monitor->init_thread(my_thread_monitor, 1,
-				       my_timer->update_mm, "update_mm",
-				       my_timer);
-	my_thread_monitor->init_thread(my_thread_monitor, 2,
-				       my_timer->update_hh, "update_hh",
-				       my_timer);
-
-	my_thread_monitor->join_threads(my_thread_monitor);
-	my_timer->show_current_time(my_timer);
-	destroy_my_thread_monitor(my_thread_monitor);
+	my_timer->start_timer(my_timer);
 	destroy_my_timer(my_timer);
 
 	return EXIT_SUCCESS;
